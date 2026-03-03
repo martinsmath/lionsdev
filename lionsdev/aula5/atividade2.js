b14cf3dffab5155471eb13f14c78d96f69e18526
Matheus Martins <128867944+martinsmath@users.noreply.github.com>
2026-03-03 00:34:38 -0300

    Add files via upload

---
+++ lionsdev/aula5/atividade2.js
@@ -0,0 +1,7 @@
+
+let nota1 = 7
+let nota2 = 8
+
+const aprovado = nota1 >= 6 && nota2 >= 6
+
+console.log(aprovado)
---
+++ lionsdev/avaliacao/matheus.js
@@ -0,0 +1,5 @@
+
+let nome = 'Beatriz'
+console.log(nome + " é gostosa demais!")
+
+console.log('eu consegui')
