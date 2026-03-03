bbd4d753060ae8cf4436e1754e0223b13ae8c17c
Matheus Martins <128867944+martinsmath@users.noreply.github.com>
2026-03-03 19:46:43 -0300

    Add files via upload

---
+++ aula5/atividade1.js
@@ -0,0 +1,23 @@
+
+// Utlize o operador de % para verificar se um número é par ou impar
+
+// Comparem se o resultado da divisão é 0 para saber se o número é par impar
+// Se o resultado da divisão for 0, o número é par
+// Se o resultado da divisão for 1, o número é impar
+
+// Utilizar o operador de === para verificar se o resto da divisão foi 0 ou 1
+
+
+const par = 10 % 2
+const impar = 10 % 2
+
+console.log(par)
+console.log(impar)
+
+const comparacao1 = par === 0
+
+console.log("10 / 2 é um número par?", comparacao1)
+
+const comparacao2 = impar === 1
+
+console.log("10 / 2 é um número impar?", comparacao2)
